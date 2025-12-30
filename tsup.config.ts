692690dd4239650fdd2e1a8d8feb7276422113aa
Chris McGravey <chris@mcgravey.dev>
2025-12-29 19:39:21 -0600

    Update for building / releasing

---
+++ tsup.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: {
+    index: "src/index.ts",
+    "anthropic/index": "src/anthropic/index.ts",
+    "openai/index": "src/openai/index.ts",
+    "google/index": "src/google/index.ts",
+    "http/index": "src/http/index.ts",
+  },
+  format: ["esm"],
+  dts: true,
+  splitting: true,
+  sourcemap: true,
+  clean: true,
+  target: "esnext",
+  outDir: "dist",
+});
